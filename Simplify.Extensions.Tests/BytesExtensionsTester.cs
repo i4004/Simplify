a1bb4c45e1963e67ff2144d84318c675139d1ac9
Alexanderius <alexander.krylkov@gmail.com>
2014-02-23 10:52:12 +0600

    Extensions added

---
+++ Simplify.Extensions.Tests/BytesExtensionsTester.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+using Simplify.Extensions.Bytes;
+
+namespace Simplify.Extensions.Tests
+{
+	[TestFixture]
+	public class BytesExtensionsTester
+	{
+		[Test]
+		public void BytesArray_GetSring_GettingCorrectly()
+		{
+			var bytes = new byte[] {116, 0, 101, 0, 115, 0, 116, 0};
+			Assert.AreEqual("test", bytes.GetString());
+		}
+	}
+}
