bdfcfb4ed2fe14b73af627dd7f037a660784c14b
Alexanderius <alexander.krylkov@gmail.com>
2014-03-11 23:51:35 +0600

    Time prodiver abstraction added

---
+++ Simplify.Core/SystemTimeProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Simplify.Core.AcspNet;
+
+namespace Simplify.Core
+{
+	/// <summary>
+	/// System time provider, returns the actual DateTime.Now, DateTime.UtcNow, DateTime.Today data
+	/// </summary>
+	public sealed class SystemTimeProvider : TimeProvider
+	{
+		/// <summary>
+		/// Gets the currrent UTC time.
+		/// </summary>
+		/// <value>
+		/// The currrent UTC time.
+		/// </value>
+		public override DateTime UtcNow
+		{
+			get { return DateTime.UtcNow; }
+		}
+
+		/// <summary>
+		/// Gets the current time.
+		/// </summary>
+		/// <value>
+		/// The current time.
+		/// </value>
+		public override DateTime Now
+		{
+			get { return DateTime.Now; }
+		}
+
+		/// <summary>
+		/// Gets the today date without time.
+		/// </summary>
+		/// <value>
+		/// The today date without time.
+		/// </value>
+		public override DateTime Today
+		{
+			get { return DateTime.Today; }
+		}
+	}
+}
---
+++ Simplify.Core/TimeProvider.cs
@@ -0,0 +1,61 @@
+﻿namespace Simplify.Core
+{
+	using System;
+
+	namespace AcspNet
+	{
+		/// <summary>
+		/// Provides Time abstraction
+		/// </summary>
+		public abstract class TimeProvider
+		{
+			private static TimeProvider CurrentInstance;
+
+			/// <summary>
+			/// Gets or sets the current time provider.
+			/// </summary>
+			/// <value>
+			/// The current time provider.
+			/// </value>
+			/// <exception cref="System.ArgumentNullException">value</exception>
+			public static TimeProvider Current
+			{
+				get
+				{
+					return CurrentInstance ?? (CurrentInstance = new SystemTimeProvider());
+				}
+				set
+				{
+					if (value == null)
+						throw new ArgumentNullException("value");
+
+					CurrentInstance = value;
+				}
+			}
+
+			/// <summary>
+			/// Gets the current UTC time.
+			/// </summary>
+			/// <value>
+			/// The current UTC time.
+			/// </value>
+			public abstract DateTime UtcNow { get; }
+
+			/// <summary>
+			/// Gets the current time.
+			/// </summary>
+			/// <value>
+			/// The current time.
+			/// </value>
+			public abstract DateTime Now { get; }
+
+			/// <summary>
+			/// Gets the today date without time.
+			/// </summary>
+			/// <value>
+			/// The today date without time.
+			/// </value>
+			public abstract DateTime Today { get; }
+		}
+	}
+}
