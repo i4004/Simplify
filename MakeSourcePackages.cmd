77bab2d5c2c3eba4e49206c0f2e52b7c2553c4b4
Alexanderius <alexander.krylkov@gmail.com>
2014-08-03 13:18:00 +0600

    Added script for creation of source packages

---
+++ MakeSourcePackages.cmd
@@ -0,0 +1,8 @@
+tools\fart.exe -r "*.cs" "public static class" "internal static class"
+
+for /F "tokens=4" %%F in ('tools\filever.exe /B /A /D src\Simplify.Xml\bin\Release\Simplify.Xml.dll') do (
+  set VERSION=%%F
+)
+
+src\.nuget\NuGet.exe pack src/Simplify.Xml/Simplify.Xml.Sources.nuspec -Version %VERSION% -OutputDirectory Publish
+Get-ChildItem .\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
