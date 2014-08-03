a40cc6fab86f38e779370fb5f17880547bf92750
Alexanderius <alexander.krylkov@gmail.com>
2014-08-03 17:57:10 +0600

    Simplify.Templates referenced to Simplify.Xml.Sources

---
+++ src/Simplify.Templates/App_Packages/Simplify.Xml.Sources.1.0.1.0/XmlExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Linq;
+
+namespace Simplify.Xml
+{
+	/// <summary>
+	/// Provides extensions for System.Xml.Linq classes
+	/// </summary>
+	internal static class XmlExtensions
+	{
+		/// <summary>
+		/// Gets the outer XML string of an XElement (inner XML and itself).
+		/// </summary>
+		/// <param name="element">The outer XML stringt.</param>
+		/// <returns></returns>
+		public static string OuterXml(this XElement element)
+		{
+			var xReader = element.CreateReader();
+			xReader.MoveToContent();
+			return xReader.ReadOuterXml();
+		}
+
+		/// <summary>
+		/// Gets the inner XML string of an XElement.
+		/// </summary>
+		/// <param name="element">The inner XML stringt.</param>
+		/// <returns></returns>
+		public static string InnerXml(this XElement element)
+		{
+			var xReader = element.CreateReader();
+			xReader.MoveToContent();
+			return xReader.ReadInnerXml();
+		}
+	}
+}
---
+++ src/Simplify.Templates/App_Packages/Simplify.Xml.Sources.1.0.1.0/XmlSerializer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Simplify.Xml
+{
+	/// <summary>
+	/// Objects XML serialization/deserialization extensions
+	/// </summary>
+	internal static class XmlSerializer
+	{
+		/// <summary>
+		/// Serializes the specified items list to a XML string.
+		/// </summary>
+		/// <typeparam name="T">Type of an item</typeparam>
+		/// <param name="items">The items list to serialize.</param>
+		/// <returns></returns>
+		public static string Serialize<T>(IList<T> items)
+		{
+			using(var memoryStream = new MemoryStream())
+			{
+				var serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<T>));
+				serializer.Serialize(memoryStream, items);
+
+				memoryStream.Position = 0;
+				return new StreamReader(memoryStream).ReadToEnd();
+			}			
+		}
+
+		/// <summary>
+		/// Serialize object to XElement.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="obj">Object to serialize.</param>
+		/// <returns></returns>
+		public static XElement ToXElement<T>(T obj)
+		{
+			using (var memoryStream = new MemoryStream())
+			{
+				using (TextWriter streamWriter = new StreamWriter(memoryStream))
+				{
+					var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+					xmlSerializer.Serialize(streamWriter, obj);
+					return XElement.Parse(Encoding.UTF8.GetString(memoryStream.ToArray()));
+				}
+			}
+		}
+
+		/// <summary>
+		/// Deserialize XElement to object.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="xElement">The XElement to deserialize.</param>
+		/// <returns></returns>
+		public static T FromXElement<T>(XElement xElement)
+		{
+			using (var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(xElement.ToString())))
+			{
+				var xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+				return (T)xmlSerializer.Deserialize(memoryStream);
+			}
+		}
+	}
+}
