71d37b632704378e05b35b747d89e44f6519160d
Alexanderius <alexander.krylkov@gmail.com>
2014-08-03 15:36:50 +0600

    Source packages creations script converted to full powershell script

--- MakeSourcePackages.ps1
+++ MakeSourcePackages.ps1
@@ -1,3 +1,5 @@
+# Patching all sources with internal attribute for use in source packages
+
 $files=get-childitem . *.cs -rec
 foreach ($file in $files)
 {
@@ -5,3 +7,15 @@ foreach ($file in $files)
 Foreach-Object {$_ -replace "public static class", "internal static class"} | 
 Set-Content -Encoding UTF8 $file.PSPath
 }
+
+# Getting packages versions
+
+$version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.Xml/bin/Release/Simplify.Xml.dll").FileVersion
+
+# Packing source packages
+
+src\.nuget\NuGet.exe pack src/Simplify.Xml/Simplify.Xml.Sources.nuspec -Version $version
+
+# Publishing to Appveyor artifacts
+
+Get-ChildItem .\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
