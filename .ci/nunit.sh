0cc0cbd490e790dd2cc376d0c05925d0db7a8052
Alexanderius <alexander.krylkov@gmail.com>
2014-08-08 09:15:07 +0600

    Travis build configuration added

---
+++ .ci/nunit.sh
@@ -0,0 +1,14 @@
+mono --runtime=v4.0 src/.nuget/NuGet.exe install NUnit.Runners -Version 2.6.3 -o packages
+
+runTest()
+{
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+   if [ $? -ne 0 ]
+   then   
+     exit 1
+   fi
+}
+
+runTest $1
+
+exit $?
