1cd005c28f82fb9d6fef6f394bdf52a3436e7fbe
Alexanderius <alexander.krylkov@gmail.com>
2014-09-01 18:44:18 +0600

    Fixes #33

--- src/Simplify.Mail/MailSenderAsyncExtensions.cs
+++ src/Simplify.Mail/MailSenderAsyncExtensions.cs
@@ -1,33 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace Simplify.Mail
 {
 	/// <summary>
-	/// Represent E-mail sending interface
+	/// Provides MailSender asynchronous operations extensions
 	/// </summary>
-	public interface IMailSender : IHideObjectMembers
+	public static class MailSenderAsyncExtensions
 	{
-		/// <summary>
-		/// MailSender settings
-		/// </summary>
-		MailSenderSettings Settings { get; }
-
-		/// <summary>
-		/// Get SMTP client with server parameters from config file and current user credentials
-		/// </summary>
-		/// <returns></returns>
-		SmtpClient SmtpClientCurrentUser { get; }
-
-		/// <summary>
-		/// Get SMTP client with credentials and server parameters from config file
-		/// </summary>
-		/// <returns></returns>
-		SmtpClient SmtpClient { get; }
-
 		/// <summary>
 		/// Send single e-mail
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="client">Smtp client</param>
 		/// <param name="from">From mail address</param>
 		/// <param name="to">Recipient e-mail address</param>
@@ -38,25 +23,30 @@ namespace Simplify.Mail
 		/// <returns>
 		/// Process status, <see langword="true" /> if message is processed to sent successfully
 		/// </returns>
-		void Send(SmtpClient client, string from, string to, string subject, string body, string bodyForAntiSpam = null,
-			params Attachment[] attachments);
+		public static Task SendAsync(this IMailSender mailSender, SmtpClient client, string from, string to, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return Task.Run(() => mailSender.Send(client, from, to, subject, body, bodyForAntiSpam, attachments));
+		}
 
 		/// <summary>
 		/// Send single e-mail using config SMTP user name and password
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="from">From mail address</param>
 		/// <param name="to">Recipient e-mail address</param>
 		/// <param name="subject">e-mail subject</param>
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if message is processed to sent successfully</returns>
-		void Send(string from, string to, string subject, string body, string bodyForAntiSpam = null,
-			params Attachment[] attachments);
+		public static Task SendAsync(this IMailSender mailSender, string from, string to, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return SendAsync(mailSender, mailSender.SmtpClient, from, to, subject, body, bodyForAntiSpam, attachments);
+		}
 
 		/// <summary>
 		/// Send e-mail to multiple recipients separately
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="client">Smtp client</param>
 		/// <param name="fromMailAddress">From mail address</param>
 		/// <param name="addresses">Recipients</param>
@@ -64,26 +54,36 @@ namespace Simplify.Mail
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if all messages are processed to sent successfully</returns>
-		void SendSeparately(SmtpClient client, string fromMailAddress, IList<string> addresses, string subject, string body,
-			string bodyForAntiSpam = null, params Attachment[] attachments);
+		/// <returns>
+		/// Process status, <see langword="true" /> if all messages are processed to sent successfully
+		/// </returns>
+		public static Task SendSeparatelyAsync(this IMailSender mailSender, SmtpClient client, string fromMailAddress, IList<string> addresses, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return Task.Run(() => mailSender.SendSeparately(client, fromMailAddress, addresses, subject, body, bodyForAntiSpam, attachments));
+		}
 
 		/// <summary>
 		/// Send e-mail to multiple recipients separately
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="fromMailAddress">From mail address</param>
 		/// <param name="addresses">Recipients</param>
 		/// <param name="subject">e-mail subject</param>
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if all messages are processed to sent successfully</returns>
-		void SendSeparately(string fromMailAddress, IList<string> addresses, string subject, string body,
-			string bodyForAntiSpam = null, params Attachment[] attachments);
+		/// <returns>
+		/// Process status, <see langword="true" /> if all messages are processed to sent successfully
+		/// </returns>
+		public static Task SendSeparatelyAsync(this IMailSender mailSender, string fromMailAddress, IList<string> addresses, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return SendSeparatelyAsync(mailSender, mailSender.SmtpClient, fromMailAddress, addresses, subject, body, bodyForAntiSpam, attachments);
+		}
 
 		/// <summary>
 		/// Send e-mail to multiple recipients in one e-mail
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="client">Smtp client</param>
 		/// <param name="fromMailAddress">From mail address</param>
 		/// <param name="addresses">Recipients</param>
@@ -91,26 +91,36 @@ namespace Simplify.Mail
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if all messages are processed to sent successfully</returns>
-		void Send(SmtpClient client, string fromMailAddress, IList<string> addresses, string subject, string body,
-			string bodyForAntiSpam = null, params Attachment[] attachments);
+		/// <returns>
+		/// Process status, <see langword="true" /> if all messages are processed to sent successfully
+		/// </returns>
+		public static Task SendAsync(this IMailSender mailSender, SmtpClient client, string fromMailAddress, IList<string> addresses, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return Task.Run(() => mailSender.Send(client, fromMailAddress, addresses, subject, body, bodyForAntiSpam, attachments));
+		}
 
 		/// <summary>
 		/// Send e-mail to multiple recipients in one e-mail
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="fromMailAddress">From mail address</param>
 		/// <param name="addresses">Recipients</param>
 		/// <param name="subject">e-mail subject</param>
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if all messages are processed to sent successfully</returns>
-		void Send(string fromMailAddress, IList<string> addresses, string subject, string body,
-			string bodyForAntiSpam = null, params Attachment[] attachments);
+		/// <returns>
+		/// Process status, <see langword="true" /> if all messages are processed to sent successfully
+		/// </returns>
+		public static Task SendAsync(this IMailSender mailSender, string fromMailAddress, IList<string> addresses, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return SendAsync(mailSender, mailSender.SmtpClient, fromMailAddress, addresses, subject, body, bodyForAntiSpam, attachments);
+		}
 
 		/// <summary>
 		/// Send e-mail to multiple recipients and carbon copy recipients in one e-mail
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="client">Smtp client</param>
 		/// <param name="fromMailAddress">From mail address</param>
 		/// <param name="addresses">Recipients</param>
@@ -119,13 +129,18 @@ namespace Simplify.Mail
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if all messages are processed to sent successfully</returns>
-		void Send(SmtpClient client, string fromMailAddress, IList<string> addresses, IList<string> ccAddresses,
-			string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments);
+		/// <returns>
+		/// Process status, <see langword="true" /> if all messages are processed to sent successfully
+		/// </returns>
+		public static Task SendAsync(this IMailSender mailSender, SmtpClient client, string fromMailAddress, IList<string> addresses, IList<string> ccAddresses, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return Task.Run(() => mailSender.Send(client, fromMailAddress, addresses, ccAddresses, subject, body, bodyForAntiSpam, attachments));
+		}
 
 		/// <summary>
 		/// Send e-mail to multiple recipients and carbon copy recipients in one e-mail
 		/// </summary>
+		/// <param name="mailSender">The mail sender.</param>
 		/// <param name="fromMailAddress">From mail address</param>
 		/// <param name="addresses">Recipients</param>
 		/// <param name="ccAddresses">Carbon copy recipients</param>
@@ -133,7 +148,13 @@ namespace Simplify.Mail
 		/// <param name="body">e-mail body</param>
 		/// <param name="bodyForAntiSpam">Part of an e-mail body just for anti-spam checking</param>
 		/// <param name="attachments">The attachments to an e-mail.</param>
-		/// <returns>Process status, <see langword="true"/> if all messages are processed to sent successfully</returns>
-		void Send(string fromMailAddress, IList<string> addresses, IList<string> ccAddresses, string subject, string body, string bodyForAntiSpam = null, params Attachment[] attachments);
+		/// <returns>
+		/// Process status, <see langword="true" /> if all messages are processed to sent successfully
+		/// </returns>
+		public static Task Send(this IMailSender mailSender, string fromMailAddress, IList<string> addresses, IList<string> ccAddresses, string subject,
+			string body, string bodyForAntiSpam = null, params Attachment[] attachments)
+		{
+			return SendAsync(mailSender, mailSender.SmtpClient, fromMailAddress, addresses, ccAddresses, subject, body, bodyForAntiSpam, attachments);
+		} 
 	}
 }
