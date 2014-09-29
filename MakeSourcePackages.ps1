ea06afe0eb8e30fd61395d602954f679800901dc
Alexanderius <alexander.krylkov@gmail.com>
2014-09-29 08:18:01 +0600

    Simplify.System.Sources packing added

--- MakeSourcePackages.ps1
+++ MakeSourcePackages.ps1
@@ -12,11 +12,13 @@ Set-Content -Encoding UTF8 $file.PSPath
 
 $xmlVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.Xml/bin/Release/Simplify.Xml.dll").FileVersion
 $stringVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.String/bin/Release/Simplify.String.dll").FileVersion
+$systemVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.System/bin/Release/Simplify.System.dll").FileVersion
 
 # Packing source packages
 
 src\.nuget\NuGet.exe pack src/Simplify.Xml/Simplify.Xml.Sources.nuspec -Version $xmlVersion
 src\.nuget\NuGet.exe pack src/Simplify.String/Simplify.String.Sources.nuspec -Version $stringVersion
+src\.nuget\NuGet.exe pack src/Simplify.System/Simplify.System.Sources.nuspec -Version $systemVersion
 
 # Publishing to Appveyor artifacts
 
