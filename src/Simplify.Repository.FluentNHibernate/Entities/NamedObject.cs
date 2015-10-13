92f354bb9bc98b023a7105fba9130355ce7924c3
Alexanderius <alexander.krylkov@gmail.com>
2015-10-13 11:48:14 +0600

    Entities implementation

---
+++ src/Simplify.Repository.FluentNHibernate/Entities/NamedObject.cs
@@ -0,0 +1,18 @@
+﻿using Simplify.Repository.Model;
+
+namespace Simplify.Repository.FluentNHibernate.Entities
+{
+	/// <summary>
+	/// Provides object with name
+	/// </summary>
+	public class NamedObject : IdentityObject, INamedObject
+	{
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>
+		/// The name.
+		/// </value>
+		public virtual string Name { get; set; }
+	}
+}
