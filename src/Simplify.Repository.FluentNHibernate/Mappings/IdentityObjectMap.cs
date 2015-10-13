b73b57279016b32ea6e72292424ccdbe05da7146
Alexanderius <alexander.krylkov@gmail.com>
2015-10-13 12:24:00 +0600

    XML comments fixed

--- src/Simplify.Repository.FluentNHibernate/Mappings/IdentityObjectMap.cs
+++ src/Simplify.Repository.FluentNHibernate/Mappings/IdentityObjectMap.cs
@@ -3,9 +3,16 @@ using Simplify.Repository.Model;
 
 namespace Simplify.Repository.FluentNHibernate.Mappings
 {
+	/// <summary>
+	/// Identity object mapping
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public class IdentityObjectMap<T> : ClassMap<T>
 		where T : IIdentityObject
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="IdentityObjectMap{T}"/> class.
+		/// </summary>
 		public IdentityObjectMap()
 		{
 			Id(x => x.ID);
