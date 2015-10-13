49d10ef5f6a1be0da630e3c38513005638366c3c
Alexanderius <alexander.krylkov@gmail.com>
2015-10-13 11:16:18 +0600

    Simplify.Repository implemented

---
+++ src/Simplify.Repository/ITransactUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace Simplify.Repository
+{
+	/// <summary>
+	/// Represent unit of work with auto-open transaction
+	/// </summary>
+	public interface ITransactUnitOfWork : IUnitOfWork
+	{
+		/// <summary>
+		/// Commits transaction.
+		/// </summary>
+		void Commit();
+
+		/// <summary>
+		/// Rollbacks transaction.
+		/// </summary>
+		void Rollback();
+	}
+}
---
+++ src/Simplify.Repository/Model/IIdentityObject.cs
@@ -0,0 +1,16 @@
+﻿namespace Simplify.Repository.Model
+{
+	/// <summary>
+	/// Represent object with identifier
+	/// </summary>
+	public interface IIdentityObject : IHideObjectMembers
+	{
+		/// <summary>
+		/// Gets the identifier.
+		/// </summary>
+		/// <value>
+		/// The identifier.
+		/// </value>
+		int ID { get; }
+	}
+}
