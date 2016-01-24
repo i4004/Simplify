7b10b7f9174949cfe3315dde6d1cf560e5831161
Alexanderius <alexander.krylkov@gmail.com>
2016-01-24 23:19:41 +0600

    Fixes #71

--- src/Simplify.FluentNHibernate/SessionExtensions.cs
+++ src/Simplify.FluentNHibernate/SessionExtensions.cs
@@ -16,10 +16,10 @@ namespace Simplify.FluentNHibernate
 		#region Single objects operations
 
 		/// <summary>
-		/// Get object from single item table
+		/// Get an object from single item table
 		/// </summary>
 		/// <typeparam name="T">Object type to get</typeparam>
-		/// <param name="session">The session.</param>
+		/// <param name="session">The NHibernate session.</param>
 		/// <returns></returns>
 		public static T GetSingleObject<T>(this ISession session)
 			where T : class
@@ -28,10 +28,10 @@ namespace Simplify.FluentNHibernate
 		}
 
 		/// <summary>
-		/// Get object from single item table
+		/// Get an object from single item table
 		/// </summary>
 		/// <typeparam name="T">Object type to get</typeparam>
-		/// <param name="session">The session.</param>
+		/// <param name="session">The NHibernate session.</param>
 		/// <param name="lockMode">The lock mode.</param>
 		/// <returns></returns>
 		public static T GetSingleObject<T>(this ISession session, LockMode lockMode)
@@ -43,55 +43,55 @@ namespace Simplify.FluentNHibernate
 		}
 
 		/// <summary>
-		/// Get object from database by filter (in case of several objects returned exception will be thrown)
+		/// Get an object from database by filter (in case of several objects returned exception will be thrown)
 		/// </summary>
-		/// <typeparam name="T">The type of object</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="filterFunc">Filtering function</param>
+		/// <typeparam name="T">The type of the object</typeparam>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="query">Query</param>
 		/// <returns></returns>
-		public static T GetObject<T>(this ISession session, Expression<Func<T, bool>> filterFunc)
+		public static T GetObject<T>(this ISession session, Expression<Func<T, bool>> query = null)
 			where T : class
 		{
 			var queryable = session.Query<T>();
 
-			if (filterFunc != null)
-				queryable = queryable.Where(filterFunc);
+			if (query != null)
+				queryable = queryable.Where(query);
 
 			return queryable.Select(x => x).SingleOrDefault();
 		}
 
 		/// <summary>
-		/// Get first object from database by filter
+		/// Get a first object from database by filter
 		/// </summary>
-		/// <typeparam name="T">The type of object</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="filterFunc">Filtering function</param>
+		/// <typeparam name="T">The type of the object</typeparam>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="query">Query</param>
 		/// <returns></returns>
-		public static T GetFirstObject<T>(this ISession session, Expression<Func<T, bool>> filterFunc)
+		public static T GetFirstObject<T>(this ISession session, Expression<Func<T, bool>> query = null)
 			where T : class
 		{
 			var queryable = session.Query<T>();
 
-			if (filterFunc != null)
-				queryable = queryable.Where(filterFunc);
+			if (query != null)
+				queryable = queryable.Where(query);
 
 			return queryable.Select(x => x).FirstOrDefault();
 		}
 
 		/// <summary>
-		/// Get and cache object from database by filter (in case of several objects returned exception will be thrown)
+		/// Get and cache an object from database by filter (in case of several objects returned exception will be thrown)
 		/// </summary>
-		/// <typeparam name="T">The type of object</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="filterFunc">Filtering function</param>
+		/// <typeparam name="T">The type of the object</typeparam>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="query">Query</param>
 		/// <returns></returns>
-		public static T GetObjectCacheable<T>(this ISession session, Expression<Func<T, bool>> filterFunc)
+		public static T GetObjectCacheable<T>(this ISession session, Expression<Func<T, bool>> query = null)
 			where T : class
 		{
 			var queryable = session.Query<T>();
 
-			if (filterFunc != null)
-				queryable = queryable.Where(filterFunc);
+			if (query != null)
+				queryable = queryable.Where(query);
 
 			queryable = queryable.Cacheable();
 
@@ -103,62 +103,62 @@ namespace Simplify.FluentNHibernate
 		#region List operations
 
 		/// <summary>
-		/// Get list of sorted objects
+		/// Get a list of sorted objects
 		/// </summary>
 		/// <typeparam name="T">The type of elements</typeparam>
-		/// <typeparam name="TOrder">Order comparing value type</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="orderFunc">Ordering function</param>
+		/// <typeparam name="TOrder">Order comparing values type</typeparam>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="orderExpression">Ordering expression</param>
 		/// <param name="orderDescending">Descending sorting</param>
 		/// <returns>List of objects</returns>
-		public static IList<T> GetSortedList<T, TOrder>(this ISession session, Expression<Func<T, TOrder>> orderFunc, bool orderDescending = false)
+		public static IList<T> GetSortedList<T, TOrder>(this ISession session, Expression<Func<T, TOrder>> orderExpression = null, bool orderDescending = false)
 		{
 			var queryable = session.Query<T>();
 
-			if (orderFunc != null)
-				queryable = orderDescending ? queryable.OrderByDescending(orderFunc) : queryable.OrderBy(orderFunc);
+			if (orderExpression != null)
+				queryable = orderDescending ? queryable.OrderByDescending(orderExpression) : queryable.OrderBy(orderExpression);
 
 			return queryable.Select(x => x).ToList();
 		}
 
 		/// <summary>
-		/// Get list of objects
+		/// Get a list of objects
 		/// </summary>
 		/// <typeparam name="T">The type of elements</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="filterFunc">Filtering function</param>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="query">Query</param>
 		/// <returns>List of objects</returns>
-		public static IList<T> GetList<T>(this ISession session, Expression<Func<T, bool>> filterFunc = null)
+		public static IList<T> GetList<T>(this ISession session, Expression<Func<T, bool>> query = null)
 			where T : class
 		{
 			var queryable = session.Query<T>();
 
-			if (filterFunc != null)
-				queryable = queryable.Where(filterFunc);
+			if (query != null)
+				queryable = queryable.Where(query);
 
 			return queryable.Select(x => x).ToList();
 		}
 
 		/// <summary>
-		/// Get list of objects
+		/// Get a list of objects
 		/// </summary>
 		/// <typeparam name="T">The type of elements</typeparam>
-		/// <typeparam name="TOrder">Order comparing value type</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="filterFunc">Filtering function</param>
-		/// <param name="orderFunc">Ordering function</param>
+		/// <typeparam name="TOrder">Order comparing values type</typeparam>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="query">Query</param>
+		/// <param name="orderExpression">Filtering expression</param>
 		/// <param name="orderDescending">Descending sorting</param>
 		/// <returns>List of objects</returns>
-		public static IList<T> GetList<T, TOrder>(this ISession session, Expression<Func<T, bool>> filterFunc, Expression<Func<T, TOrder>> orderFunc, bool orderDescending = false)
+		public static IList<T> GetList<T, TOrder>(this ISession session, Expression<Func<T, bool>> query = null, Expression<Func<T, TOrder>> orderExpression = null, bool orderDescending = false)
 			where T : class
 		{
 			var queryable = session.Query<T>();
 
-			if (filterFunc != null)
-				queryable = queryable.Where(filterFunc);
+			if (query != null)
+				queryable = queryable.Where(query);
 
-			if (orderFunc != null)
-				queryable = orderDescending ? queryable.OrderByDescending(orderFunc) : queryable.OrderBy(orderFunc);
+			if (orderExpression != null)
+				queryable = orderDescending ? queryable.OrderByDescending(orderExpression) : queryable.OrderBy(orderExpression);
 
 			return queryable.Select(x => x).ToList();
 		}
@@ -168,17 +168,17 @@ namespace Simplify.FluentNHibernate
 		/// </summary>
 		/// <typeparam name="T">The type of elements</typeparam>
 		/// <typeparam name="TOrder">Order comparing value type</typeparam>
-		/// <param name="session">The nhibernate session.</param>
-		/// <param name="orderFunc">Ordering function</param>
+		/// <param name="session">The NHibernate session.</param>
+		/// <param name="orderExpression">Ordering function</param>
 		/// <param name="orderDescending">Descending sorting</param>
 		/// <returns>Sorted list of objects</returns>
-		public static IList<T> GetListSorted<T, TOrder>(this ISession session, Expression<Func<T, TOrder>> orderFunc, bool orderDescending = false)
+		public static IList<T> GetListSorted<T, TOrder>(this ISession session, Expression<Func<T, TOrder>> orderExpression = null, bool orderDescending = false)
 			where T : class
 		{
 			var queryable = session.Query<T>();
 
-			if (orderFunc != null)
-				queryable = orderDescending ? queryable.OrderByDescending(orderFunc) : queryable.OrderBy(orderFunc);
+			if (orderExpression != null)
+				queryable = orderDescending ? queryable.OrderByDescending(orderExpression) : queryable.OrderBy(orderExpression);
 
 			return queryable.Select(x => x).ToList();
 		}
