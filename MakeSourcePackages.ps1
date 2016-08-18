3912720027632f95616ea36d4432167e08213627
Alexanderius <alexander.krylkov@gmail.com>
2016-08-18 15:48:11 +0600

    Simplify extensions sources package build

--- MakeSourcePackages.ps1
+++ MakeSourcePackages.ps1
@@ -16,12 +16,14 @@ Set-Content -Encoding UTF8 $file.PSPath
 $xmlVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.Xml/bin/Release/Simplify.Xml.dll").FileVersion
 $stringVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.String/bin/Release/Simplify.String.dll").FileVersion
 $systemVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.System/bin/Release/Simplify.System.dll").FileVersion
+$extensionsVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot/src/Simplify.Extensions/bin/Release/Simplify.Extensions.dll").FileVersion
 
 # Packing source packages
 
 src\.nuget\NuGet.exe pack src/Simplify.Xml/Simplify.Xml.Sources.nuspec -Version $xmlVersion
 src\.nuget\NuGet.exe pack src/Simplify.String/Simplify.String.Sources.nuspec -Version $stringVersion
 src\.nuget\NuGet.exe pack src/Simplify.System/Simplify.System.Sources.nuspec -Version $systemVersion
+src\.nuget\NuGet.exe pack src/Simplify.System/Simplify.Extensions.Sources.nuspec -Version $extensionsVersion
 
 # Publishing to Appveyor artifacts
 
