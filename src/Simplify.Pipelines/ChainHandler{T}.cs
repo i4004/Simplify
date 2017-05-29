99116f63218e712f58b2eaf5dfa7a08f935e6a95
Alexanderius <alexander.krylkov@gmail.com>
2017-05-29 16:43:34 +0600

    Chain of responsibility base class added

---
+++ src/Simplify.Pipelines/ChainHandler{T}.cs
@@ -0,0 +1,35 @@
+﻿namespace Simplify.Pipelines
+{
+	/// <summary>
+	/// Provides chain of responsibility handler base class
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public abstract class ChainHandler<T>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ChainHandler{T}"/> class.
+		/// </summary>
+		/// <param name="successor">The successor handler.</param>
+		protected ChainHandler(ChainHandler<T> successor = null)
+		{
+			Successor = successor;
+		}
+
+		/// <summary>
+		/// Gets the successor handler.
+		/// </summary>
+		/// <value>
+		/// The successor.
+		/// </value>
+		public ChainHandler<T> Successor { get; }
+
+		/// <summary>
+		/// Executes the handler.
+		/// </summary>
+		/// <param name="args">The arguments.</param>
+		public virtual void Execute(T args)
+		{
+			Successor?.Execute(args);
+		}
+	}
+}
