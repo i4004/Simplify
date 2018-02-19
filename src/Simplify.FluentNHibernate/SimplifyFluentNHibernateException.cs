d777384e76ca43e1831872378039f1c6777caf6a
Alexanderius <alexander.krylkov@gmail.com>
2018-02-19 23:28:43 +0600

    SimplifyFluentNHibernateException implemented

---
+++ src/Simplify.FluentNHibernate/SimplifyFluentNHibernateException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Simplify.FluentNHibernate
+{
+	/// <summary>
+	/// Provides Simplify.FluentNHibernate related exception
+	/// </summary>
+	/// <seealso cref="Exception" />
+	public class SimplifyFluentNHibernateException : Exception
+	{
+		public SimplifyFluentNHibernateException()
+		{
+		}
+
+		public SimplifyFluentNHibernateException(string message) : base(message)
+		{
+		}
+
+		public SimplifyFluentNHibernateException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
