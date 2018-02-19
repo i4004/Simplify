da04e33f8304d0621e2892ec3cbe75bb44c31f18
Alexanderius <alexander.krylkov@gmail.com>
2018-02-19 23:29:12 +0600

    OracleDataClientDriverFix update to NHibernate 5.0, refactored

--- src/Simplify.FluentNHibernate/Drivers/OracleDataClientDriverFix.cs
+++ src/Simplify.FluentNHibernate/Drivers/OracleDataClientDriverFix.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Data;
-
+using System.Data.Common;
 using NHibernate.Driver;
 using NHibernate.SqlTypes;
 using NHibernate.Util;
@@ -10,7 +9,7 @@ namespace Simplify.FluentNHibernate.Drivers
 	/// <summary>
 	/// OracleDataClientDriver with NCLOB/CLOBs IDbDataParameter.Value = (string whose length: 4000 > length > 2000 ) bug fix
 	/// </summary>
-	class OracleDataClientDriverFix : OracleDataClientDriver
+	internal class OracleDataClientDriverFix : OracleDataClientDriver
 	{
 		public const string DriverAssemblyName = "Oracle.DataAccess";
 		private const string OracleParameterType = "Oracle.DataAccess.Client.OracleParameter";
@@ -22,18 +21,21 @@ namespace Simplify.FluentNHibernate.Drivers
 		/// <param name="dbParam">The db param.</param>
 		/// <param name="name">The name.</param>
 		/// <param name="sqlType">Type of the SQL.</param>
-		protected override void InitializeParameter(IDbDataParameter dbParam, string name, SqlType sqlType)
+		protected override void InitializeParameter(DbParameter dbParam, string name, SqlType sqlType)
 		{
 			base.InitializeParameter(dbParam, name, sqlType);
 
-			if ((sqlType is StringClobSqlType))
-			{
-				ReflectHelper.TypeFromAssembly(OracleParameterType, DriverAssemblyName, false)
-					.GetProperty("OracleDbType")
-					.SetValue(dbParam,
-						Enum.Parse(ReflectHelper.TypeFromAssembly(OracleDbType, DriverAssemblyName, false), "NClob"), null);
-			}
-		}
+			if (!(sqlType is StringClobSqlType))
+				return;
 
+			var property = ReflectHelper.TypeFromAssembly(OracleParameterType, DriverAssemblyName, false)
+				.GetProperty("OracleDbType");
+
+			if (property == null)
+				throw new SimplifyFluentNHibernateException("OracleDbType property is not found");
+
+			property.SetValue(dbParam,
+				Enum.Parse(ReflectHelper.TypeFromAssembly(OracleDbType, DriverAssemblyName, false), "NClob"), null);
+		}
 	}
 }
