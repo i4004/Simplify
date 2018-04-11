42e4642144fc80e13bd276c497a80b99a432d976
Alexanderius <alexander.krylkov@gmail.com>
2018-04-11 23:25:03 +0600

    Cleanup

--- src/Simplify.Resources/Tools/Install.ps1
+++
@@ -1,3 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project.Object.References.Add("$installPath\lib\net40\Simplify.Resources.dll")
--- src/Simplify.Resources/Tools/Uninstall.ps1
+++
@@ -1,3 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project.Object.References | where { $_.Name -eq 'Simplify.Resources' } | foreach { $_.Remove() }
