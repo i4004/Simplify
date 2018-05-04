1fc404cc3a514af93206b72aab7f49fd7ff9126a
Alexanderius <alexander.krylkov@gmail.com>
2018-05-04 19:52:44 +0600

    .NET Standard support set, dependency on Simplify.FluentNHibernate.Examples.Database removed to prevent packages dependencies versions problems

---
+++ src/Simplify.FluentNHibernate.Tests/Entities/Accounts/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simplify.FluentNHibernate.Tests.Entities.Accounts
+{
+	public class User
+	{
+		public virtual int ID { get; set; }
+
+		public virtual string Name { get; set; }
+
+		public virtual string Password { get; set; }
+
+		public virtual string EMail { get; set; }
+
+		public virtual DateTime LastActivityTime { get; set; }
+	}
+}
--- src/Simplify.FluentNHibernate.Tests/SessionExtensionsTests.cs
+++ src/Simplify.FluentNHibernate.Tests/SessionExtensionsTests.cs
@@ -7,8 +7,8 @@ using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
 
 using NUnit.Framework;
-using Simplify.FluentNHibernate.Examples.Database.Entities.Accounts;
-using Simplify.FluentNHibernate.Examples.Database.Mappings;
+using Simplify.FluentNHibernate.Tests.Entities.Accounts;
+using Simplify.FluentNHibernate.Tests.Mappings.Accounts;
 
 namespace Simplify.FluentNHibernate.Tests
 {
