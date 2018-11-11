a0d04659b5857166acb7d68ae6ef5444de5969c6
Alexanderius <alexander.krylkov@gmail.com>
2018-11-11 11:44:25 +0600

    Test comments

--- src/Simplify.Xml.Tests/XmlExtensionsTests.cs
+++ src/Simplify.Xml.Tests/XmlExtensionsTests.cs
@@ -9,16 +9,20 @@ namespace Simplify.Xml.Tests
 		[Test]
 		public void GetOuterXml_XElement_GettingCorrectly()
 		{
+			// Assign
 			var element = XElement.Parse("<test><foo>data</foo></test>");
 
+			// Act & Assert
 			Assert.AreEqual("<test><foo>data</foo></test>", element.OuterXml());
 		}
 
 		[Test]
 		public void GetInnerXml_XElement_GettingCorrectly()
 		{
+			// Assign
 			var element = XElement.Parse("<test><foo>data</foo></test>");
 
+			// Act & Assert
 			Assert.AreEqual("<foo>data</foo>", element.InnerXml());
 		}
 	}
