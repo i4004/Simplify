0262f2c2cd00da59c4ea4b54bca640aca093b9cc
Alexanderius <alexander.krylkov@gmail.com>
2018-12-10 22:22:50 +0600

    Initial Core test app

---
+++ src/Simplify.Mail.TestConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simplify.Mail.TestConsoleApp
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
