8266dd5231cc16ca102722e8f304fb53ffd1ce57
Alexanderius <alexander.krylkov@gmail.com>
2018-12-10 21:57:01 +0600

    SendSimpleTestEmail example added

---
+++ src/Simplify.Mail.IntegrationTests/MailSenderTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Simplify.Mail.IntegrationTests
+{
+	// For test SMTP server you can use https://github.com/rnwood/smtp4dev
+	[TestFixture]
+	public class MailSenderTests
+	{
+		[Test]
+		public void SendSimpleTestEmail()
+		{
+			MailSender.Default.Send("somesender@somedomain.com", "somereceiver@somedomain.com", "Hello subject", "Hello World!!!");
+		}
+	}
+}
