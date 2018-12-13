6fdddb327ff9a1178de980d20cb07060736c6b81
Alexanderius <alexander.krylkov@gmail.com>
2018-12-13 21:35:15 +0600

    Null check, documentation fix
    MySQL, MS SQL, PostgreSQL IConfiguration based impl

--- src/Simplify.FluentNHibernate/ConfigurationExtensions.cs
+++ src/Simplify.FluentNHibernate/ConfigurationExtensions.cs
@@ -43,8 +43,8 @@ namespace Simplify.FluentNHibernate
 		/// Initialize Oracle connection using Oracle10 client configuration and using oracle client to connect to database
 		/// </summary>
 		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
-		/// <param name="configuration">The database configuration.</param>
-		/// <param name="configSectionName">Configuration section name in configuration.</param>
+		/// <param name="configuration">The configuration containing database config section.</param>
+		/// <param name="configSectionName">Database configuration section name in configuration.</param>
 		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
 		/// <returns></returns>
 		/// <exception cref="ArgumentNullException">fluentConfiguration
@@ -112,17 +112,22 @@ namespace Simplify.FluentNHibernate
 		/// Initialize Oracle connection using Oracle10 client configuration and using Oracle.DataAccess.dll to connect to database
 		/// </summary>
 		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
-		/// <param name="configuration">The database configuration.</param>
-		/// <param name="configSectionName">Configuration section name in configuration.</param>
+		/// <param name="configuration">The configuration containing database config section.</param>
+		/// <param name="configSectionName">Database configuration section name in configuration.</param>
 		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
 		/// <returns></returns>
-		/// <exception cref="ArgumentNullException">fluentConfiguration</exception>
+		/// <exception cref="ArgumentNullException">
+		/// fluentConfiguration
+		/// or
+		/// configuration
+		/// </exception>
 		public static FluentConfiguration InitializeFromConfigOracleOdpNetNative(this FluentConfiguration fluentConfiguration,
 			IConfiguration configuration,
 			string configSectionName = "DatabaseConnectionSettings",
 			Action<OracleDataClientConfiguration> additionalClientConfiguration = null)
 		{
 			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
+			if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
 			InitializeFromConfigOracleOdpNetNative(fluentConfiguration,
 				new ConfigurationBasedDbConnectionSettings(configuration, configSectionName),
@@ -179,17 +184,22 @@ namespace Simplify.FluentNHibernate
 		/// Initialize Oracle connection using Oracle10 client configuration and using Oracle.ManagedDataAccess.dll to connect to database
 		/// </summary>
 		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
-		/// <param name="configuration">The database configuration.</param>
-		/// <param name="configSectionName">Configuration section name in configuration.</param>
+		/// <param name="configuration">The configuration containing database config section.</param>
+		/// <param name="configSectionName">Database configuration section name in configuration.</param>
 		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
 		/// <returns></returns>
-		/// <exception cref="ArgumentNullException">fluentConfiguration</exception>
+		/// <exception cref="ArgumentNullException">
+		/// fluentConfiguration
+		/// or
+		/// configuration
+		/// </exception>
 		public static FluentConfiguration InitializeFromConfigOracleOdpNet(this FluentConfiguration fluentConfiguration,
 			IConfiguration configuration,
 			string configSectionName = "DatabaseConnectionSettings",
 			Action<OracleClientConfiguration> additionalClientConfiguration = null)
 		{
 			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
+			if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
 			InitializeFromConfigOracleOdpNet(fluentConfiguration,
 				new ConfigurationBasedDbConnectionSettings(configuration, configSectionName),
@@ -219,22 +229,61 @@ namespace Simplify.FluentNHibernate
 
 		#endregion ODP.NET
 
+		#region MySQL
+
 		/// <summary>
 		/// Initialize MySQL connection using Standard client configuration
 		/// </summary>
-		/// <param name="configuration">The fluentNHibernate configuration.</param>
-		/// <param name="configSectionName">Configuration section name in App.config or Web.config file</param>
+		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
+		/// <param name="configSectionName">Configuration section name in App.config or Web.config file.</param>
 		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
 		/// <returns></returns>
-		/// <exception cref="ArgumentNullException"></exception>
-		public static FluentConfiguration InitializeFromConfigMySql(this FluentConfiguration configuration,
+		/// <exception cref="ArgumentNullException">fluentConfiguration</exception>
+		public static FluentConfiguration InitializeFromConfigMySql(this FluentConfiguration fluentConfiguration,
 			string configSectionName = "DatabaseConnectionSettings",
 			Action<MySQLConfiguration> additionalClientConfiguration = null)
 		{
+			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
+
+			InitializeFromConfigMySql(fluentConfiguration,
+				new ConfigurationManagerBasedDbConnectionSettings(configSectionName),
+				additionalClientConfiguration);
+
+			return fluentConfiguration;
+		}
+
+		/// <summary>
+		/// Initialize MySQL connection using Standard client configuration
+		/// </summary>
+		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
+		/// <param name="configuration">The configuration containing database config section.</param>
+		/// <param name="configSectionName">Database configuration section name in configuration.</param>
+		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException">
+		/// fluentConfiguration
+		/// or
+		/// configuration
+		/// </exception>
+		public static FluentConfiguration InitializeFromConfigMySql(this FluentConfiguration fluentConfiguration,
+			IConfiguration configuration,
+			string configSectionName = "DatabaseConnectionSettings",
+			Action<MySQLConfiguration> additionalClientConfiguration = null)
+		{
+			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
 			if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
-			var settings = new ConfigurationManagerBasedDbConnectionSettings(configSectionName);
+			InitializeFromConfigMySql(fluentConfiguration,
+				new ConfigurationBasedDbConnectionSettings(configuration, configSectionName),
+				additionalClientConfiguration);
 
+			return fluentConfiguration;
+		}
+
+		private static void InitializeFromConfigMySql(FluentConfiguration fluentConfiguration,
+			DbConnectionSettings settings,
+			Action<MySQLConfiguration> additionalClientConfiguration = null)
+		{
 			var clientConfiguration = MySQLConfiguration.Standard.ConnectionString(c => c
 				.Server(settings.ServerName)
 				.Database(settings.DataBaseName)
@@ -243,31 +292,68 @@ namespace Simplify.FluentNHibernate
 
 			additionalClientConfiguration?.Invoke(clientConfiguration);
 
-			configuration.Database(clientConfiguration);
-			configuration.ExposeConfiguration(c => c.Properties.Add("hbm2ddl.keywords", "none"));
+			fluentConfiguration.Database(clientConfiguration);
 
-			if (settings.ShowSql)
-				configuration.ExposeConfiguration(x => x.SetInterceptor(new SqlStatementInterceptor()));
+			PerformCommonInitialization(fluentConfiguration, settings);
+		}
 
-			return configuration;
+		#endregion MySQL
+
+		#region MS SQL
+
+		/// <summary>
+		/// Initialize MsSQL connection using MsSql2008 client configuration
+		/// </summary>
+		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
+		/// <param name="configSectionName">Configuration section name in App.config or Web.config file.</param>
+		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		public static FluentConfiguration InitializeFromConfigMsSql(this FluentConfiguration fluentConfiguration,
+			string configSectionName = "DatabaseConnectionSettings",
+			Action<MsSqlConfiguration> additionalClientConfiguration = null)
+		{
+			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
+
+			InitializeFromConfigMsSql(fluentConfiguration,
+				new ConfigurationManagerBasedDbConnectionSettings(configSectionName),
+				additionalClientConfiguration);
+
+			return fluentConfiguration;
 		}
 
 		/// <summary>
 		/// Initialize MsSQL connection using MsSql2008 client configuration
 		/// </summary>
-		/// <param name="configuration">The fluentNHibernate configuration.</param>
-		/// <param name="configSectionName">Configuration section name in App.config or Web.config file</param>
+		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
+		/// <param name="configuration">The configuration containing database config section.</param>
+		/// <param name="configSectionName">Database configuration section name in configuration.</param>
 		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
 		/// <returns></returns>
-		/// <exception cref="ArgumentNullException"></exception>
-		public static FluentConfiguration InitializeFromConfigMsSql(this FluentConfiguration configuration,
+		/// <exception cref="ArgumentNullException">
+		/// fluentConfiguration
+		/// or
+		/// configuration
+		/// </exception>
+		public static FluentConfiguration InitializeFromConfigMsSql(this FluentConfiguration fluentConfiguration,
+			IConfiguration configuration,
 			string configSectionName = "DatabaseConnectionSettings",
 			Action<MsSqlConfiguration> additionalClientConfiguration = null)
 		{
+			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
 			if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
-			var settings = new ConfigurationManagerBasedDbConnectionSettings(configSectionName);
+			InitializeFromConfigMsSql(fluentConfiguration,
+				new ConfigurationBasedDbConnectionSettings(configuration, configSectionName),
+				additionalClientConfiguration);
 
+			return fluentConfiguration;
+		}
+
+		private static void InitializeFromConfigMsSql(FluentConfiguration fluentConfiguration,
+			DbConnectionSettings settings,
+			Action<MsSqlConfiguration> additionalClientConfiguration = null)
+		{
 			var clientConfiguration = MsSqlConfiguration.MsSql2008.ConnectionString(c => c
 				.Server(settings.ServerName)
 				.Database(settings.DataBaseName)
@@ -276,31 +362,68 @@ namespace Simplify.FluentNHibernate
 
 			additionalClientConfiguration?.Invoke(clientConfiguration);
 
-			configuration.Database(clientConfiguration);
-			configuration.ExposeConfiguration(c => c.Properties.Add("hbm2ddl.keywords", "none"));
+			fluentConfiguration.Database(clientConfiguration);
 
-			if (settings.ShowSql)
-				configuration.ExposeConfiguration(x => x.SetInterceptor(new SqlStatementInterceptor()));
+			PerformCommonInitialization(fluentConfiguration, settings);
+		}
 
-			return configuration;
+		#endregion MS SQL
+
+		#region PostgreSQL
+
+		/// <summary>
+		/// Initialize PostgreSQL connection using PostgreSQL82 client configuration
+		/// </summary>
+		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
+		/// <param name="configSectionName">Configuration section name in App.config or Web.config file.</param>
+		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		public static FluentConfiguration InitializeFromConfigPostgreSql(this FluentConfiguration fluentConfiguration,
+			string configSectionName = "DatabaseConnectionSettings",
+			Action<PostgreSQLConfiguration> additionalClientConfiguration = null)
+		{
+			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
+
+			InitializeFromConfigPostgreSql(fluentConfiguration,
+				new ConfigurationManagerBasedDbConnectionSettings(configSectionName),
+				additionalClientConfiguration);
+
+			return fluentConfiguration;
 		}
 
 		/// <summary>
 		/// Initialize PostgreSQL connection using PostgreSQL82 client configuration
 		/// </summary>
-		/// <param name="configuration">The fluentNHibernate configuration.</param>
-		/// <param name="configSectionName">Configuration section name in App.config or Web.config file</param>
+		/// <param name="fluentConfiguration">The fluentNHibernate configuration.</param>
+		/// <param name="configuration">The configuration containing database config section.</param>
+		/// <param name="configSectionName">Database configuration section name in configuration.</param>
 		/// <param name="additionalClientConfiguration">The additional client configuration.</param>
 		/// <returns></returns>
-		/// <exception cref="ArgumentNullException"></exception>
-		public static FluentConfiguration InitializeFromConfigPostgreSql(this FluentConfiguration configuration,
+		/// <exception cref="ArgumentNullException">
+		/// fluentConfiguration
+		/// or
+		/// configuration
+		/// </exception>
+		public static FluentConfiguration InitializeFromConfigPostgreSql(this FluentConfiguration fluentConfiguration,
+			IConfiguration configuration,
 			string configSectionName = "DatabaseConnectionSettings",
 			Action<PostgreSQLConfiguration> additionalClientConfiguration = null)
 		{
+			if (fluentConfiguration == null) throw new ArgumentNullException(nameof(fluentConfiguration));
 			if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
-			var settings = new ConfigurationManagerBasedDbConnectionSettings(configSectionName);
+			InitializeFromConfigPostgreSql(fluentConfiguration,
+				new ConfigurationBasedDbConnectionSettings(configuration, configSectionName),
+				additionalClientConfiguration);
 
+			return fluentConfiguration;
+		}
+
+		private static void InitializeFromConfigPostgreSql(FluentConfiguration fluentConfiguration,
+			DbConnectionSettings settings,
+			Action<PostgreSQLConfiguration> additionalClientConfiguration = null)
+		{
 			var clientConfiguration = PostgreSQLConfiguration.PostgreSQL82.ConnectionString(c => c
 				.Host(settings.ServerName)
 				.Port(settings.Port ?? 5432)
@@ -310,14 +433,12 @@ namespace Simplify.FluentNHibernate
 
 			additionalClientConfiguration?.Invoke(clientConfiguration);
 
-			configuration.Database(clientConfiguration);
-			configuration.ExposeConfiguration(c => c.Properties.Add("hbm2ddl.keywords", "none"));
+			fluentConfiguration.Database(clientConfiguration);
 
-			if (settings.ShowSql)
-				configuration.ExposeConfiguration(x => x.SetInterceptor(new SqlStatementInterceptor()));
-
-			return configuration;
+			PerformCommonInitialization(fluentConfiguration, settings);
 		}
+
+		#endregion PostgreSQL
 
 		/// <summary>
 		/// Initialize SqLite connection using Standard client configuration
