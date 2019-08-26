e5eef12d95e4867e096098ce2ce90abd0a646fa7
Alexanderius <alexander.krylkov@gmail.com>
2019-08-26 22:50:09 +0600

    Initial acrhitecture for #152

---
+++ src/Simplify.DI.AspNetCore/DIRegistratorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Simplify.DI.AspNetCore
+{
+	/// <summary>
+	/// Provides IDIRegistrator extensions for Microsoft.Extensions.DependencyInjection integration
+	/// </summary>
+	public static class DIRegistratorExtensions
+	{
+		/// <summary>
+		/// Registers the types from IServiceCollection inside IDIRegistrator (IDIContainerProvider).
+		/// </summary>
+		/// <param name="registrator">The registrator.</param>
+		/// <param name="collection">The collection.</param>
+		/// <exception cref="ArgumentNullException">collection</exception>
+		public static void RegisterFromServiceCollection(this IDIRegistrator registrator, IServiceCollection collection)
+		{
+			if (registrator == null) throw new ArgumentNullException(nameof(registrator));
+			if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+			foreach (var item in collection)
+			{
+				// TODO
+				//registrator.Register(item.ServiceType);
+			}
+		}
+	}
+}
